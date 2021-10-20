0820b0235641bd2915140dfda576760871f27b89
Miguel Barahona <miguelbarahona00@gmail.com>
2021-10-19 21:56:01 -0500

    Arrow functions ES6

--- 07-ES6/js/app.js
+++ 07-ES6/js/app.js
@@ -25,20 +25,20 @@ console.log(dateOfBirthES6);
 let age;
 age = 19;
 
-function ChangeValue(){
+function ChangeValue() {
     age = 50;
 }
-ChangeValue();  
+ChangeValue();
 console.log('Edad ' + age);
 
 let age1;
 age1 = 25;
 
-function ChangeValue(){
+function ChangeValue() {
     let age1;
     age1 = 50;
 }
-ChangeValue();  
+ChangeValue();
 console.log('Edad ' + age1);
 
 {
@@ -55,7 +55,7 @@ let firstNameES6 = 'Carlos';
 let lastNameES6 = 'Cardenas';
 const dateOfBirth = 1990;
 
-function Age(){
+function Age() {
     return new Date().getFullYear() - dateOfBirth;
 }
 
@@ -71,3 +71,75 @@ console.log(firstNameES6.includes('Cas'));
 
 console.log(firstNameES6.startsWith('Ca'));
 console.log(firstNameES6.endsWith('os'));
+
+// Funciones de flecha
+
+let years = [2000, 2004, 2009, 2013];
+
+//ES5
+var ageES5 = years.map(function (year) {
+    return new Date().getFullYear() - year;
+});
+console.log(ageES5);
+
+//ES6
+var ageES6 = years.map(year => new Date().getFullYear() - year);
+console.log(ageES6);
+
+let ageES6Example1 = years.map((year, index) => `La edad del usuario ${index + 1} es de ${new Date().getFullYear() - year}`);
+
+console.log(ageES6Example1);
+
+let ageES6Example2 = years.map((year, index) => {
+    let age = new Date().getFullYear() - year;
+    return `La edad del usuario ${index + 1} es de ${age}`;
+});
+
+console.log(ageES6Example2);
+
+//Example 2 => Flecha
+
+
+//ES5
+const cuadradoES5 = function CuadradoES5(num){
+    return num * num;
+}
+
+console.log(cuadradoES5(5));
+
+//ES6
+const cuadradoES6Example1 = (num) => num * num;
+console.log(cuadradoES6Example1(3));
+
+const cuadradoES6Example2 = (num) => {
+    return num * num;
+};
+console.log(cuadradoES6Example2(6));
+
+const people = [
+    {
+        Name: 'Carlos',
+        Age: 26
+    },
+    {
+        Name: 'Camilo',
+        Age: 15
+    },
+    {
+        Name: 'Alexander',
+        Age: 17
+    },
+    {
+        Name: 'Alejandra',
+        Age: 18
+    }
+]
+
+const personES5 = people.filter(function(person){
+    return person.Age < 18
+})
+
+console.log(personES5);
+
+const personES6 = people.filter((person) => person.Age >= 18);
+console.log(personES6);
